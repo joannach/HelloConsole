b80d311650288ff53754ff4a18f3a69280f50e53
joannach <joanna.chrominska9@gmail.com>
2023-04-23 16:30:58 +0200

    Add project files.

---
+++ HelloConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username: ");
+            var username = Console.ReadLine();
+
+            Console.WriteLine(string.Format("Hello, {0}", username));
+        }
+    }
+}
