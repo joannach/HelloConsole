46f09064a2407cd0a4b710e810ac921931cf44a4
joannach <joanna.chrominska9@gmail.com>
2023-04-25 20:24:36 +0200

    Add WPF project displays Hello {username}

---
+++ HelloWPF/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using Windows.UI.Xaml.Controls;
+
+namespace HelloWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new HelloWindow(textBox_username.Text);
+            dialog.ShowDialog();
+        }
+
+        private void button_close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
